490fc9a7385994611a4a865299e860d1cb432700
bhargavsk1077 <bhargavskumar51703@gmail.com>
2020-04-16 19:00:37 +0530

    heroku deployment changes

--- config.py
+++ config.py
@@ -11,3 +11,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = os.environ.get('ADMINS')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
